eea276618a38196decbe0845336e30e5d49b9e3b
steinnhauser <steinnhauser@mac.com>
2018-09-07 13:17:21 +0200

    Added LU-decomposition into Project1

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -4,82 +4,175 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include <time.h>
+#include <armadillo>
 using namespace std;
+using namespace arma;
 ofstream ofile;
+
+void ThomasAlgorithm(string fname, int maxexp);
+void LUDecompFunc(string fname, int maxexp);
+
 //First, build vectors d and b.
 //Segmentation fault... sjekk indexer
 //Hvis ikke, skriv .at(index)
 int main(int argc, char* argv[])
 {
+    string fname;
+    int maxexp;
+    fname=argv[1];
+    maxexp = atoi(argv[2]);
+
+    //ThomasAlgorithm(fname, maxexp);
+    LUDecompFunc(fname, maxexp);
+
     //Variable n.
     //Initialize matrix vectors, saving ram space.
     //double d_vec[n+1], dt_vec[n+1], f_vec[n+1], ft_vec[n+1], a_vec[n+1], c_vec[n+1], u_vec[n+1];
+}
 
-    string fname;
-    int maxexp;
-    maxexp = atoi(argv[2]);
 
-    for(int i=1; i<=maxexp; i++){
-      fname=argv[1];
+void ThomasAlgorithm(string fname, int maxexp){
+  //string fname;
+  //int maxexp;
+  //maxexp = atoi(argv[2]);
+  clock_t start, finish;
+  string filename;
 
-      int n;
-      n=pow(10.0, i);
-      fname.append(to_string(n));
-      double *d_vec = new double [n+2]; double *dt_vec = new double [n+2]; double *f_vec = new double [n+2];
-      double *ft_vec = new double [n+2]; double *a_vec = new double [n+1]; double *c_vec = new double [n+1]; double *u_vec = new double [n+2];
+  for(int i=1; i<=maxexp; i++){
+    filename=fname;
 
-      //double d_vec[n+2]; double dt_vec[n+2]; double f_vec[n+2]; double ft_vec[n+2]; double a_vec[n+1]; double c_vec[n+1]; double u_vec[n+2];
+    int n;
+    n=pow(10.0, i);
+    filename.append(to_string(i));
+    double *d_vec = new double [n+2]; double *dt_vec = new double [n+2]; double *f_vec = new double [n+2];
+    double *ft_vec = new double [n+2]; double *a_vec = new double [n+1]; double *c_vec = new double [n+1]; double *u_vec = new double [n+2];
 
-      double x0=0, h, x;
-      h=1/(double(n+1));
+    //double d_vec[n+2]; double dt_vec[n+2]; double f_vec[n+2]; double ft_vec[n+2]; double a_vec[n+1]; double c_vec[n+1]; double u_vec[n+2];
 
-      //Fill f array analytically:
-      for (int i=0; i<=n+1; i++){
-          x=x0+h*double(i);
-          f_vec[i]=h*h*100*exp(-10*x);
-      }
-      //Special case diagonals
-      for(int i=1; i<=n; i++){
-          d_vec[i]=2;
-          a_vec[i]=-1;
-          c_vec[i]=-1;
-          //dt_vec[i]=0;
-          //ft_vec[i]=0;
-          //u_vec[i]=0;       //Initial values of u: u(0)=u(1)=0
-      }
+    double x0=0, h, x;
+    h=1/(double(n+1));
 
-      dt_vec[1]=d_vec[1];
-      ft_vec[1]=f_vec[1];
-      u_vec[0]=0;
-      u_vec[n+1]=0;
-
-      //First for-loop to create b vector.
-      for(int i=2; i<=n; i++){
-          dt_vec[i]=d_vec[i] - c_vec[i-1]*a_vec[i-1]/dt_vec[i-1];
-          ft_vec[i]=f_vec[i] - ft_vec[i-1]*a_vec[i-1]/dt_vec[i-1];
-      }
-      u_vec[n]=ft_vec[n]/dt_vec[n];
-
-      //Compute u=f/d:j
-      for(int i=n; i>0; i--){
-          u_vec[i]=(ft_vec[i]-c_vec[i]*u_vec[i+1])/dt_vec[i];
-      }
-      //write file with current i and n:
-      fname.append(".txt");
-      ofile.open(fname);
-      //ofile << setiosflags(ios::showpoint | ios::uppercase);
-      for(double i=1; i<=n; i++){
-          double h;
-          h=1/(double(n)+1);
-          ofile << setprecision(10) << setw(20) << h*i;
-          ofile << setprecision(10) << setw(20) << 1-(1-exp(-10))*h*i - exp(-10*h*i);
-          ofile << setprecision(10) << setw(20) << u_vec[int(i)];
-          ofile << setprecision(10) << setw(20) << log10(fabs((- (1-(1-exp(-10))*h*i - exp(-10*h*i)) + u_vec[int(i)])/(1-(1-exp(-10))*h*i - exp(-10*h*i)))) << endl;
-      }
-      ofile.close();
-      delete [] d_vec; delete [] dt_vec; delete [] f_vec; delete [] ft_vec;
-      delete [] a_vec; delete [] c_vec; delete [] u_vec;
+    //Fill f array analytically:
+    for (int i=0; i<=n+1; i++){
+        x=x0+h*double(i);
+        f_vec[i]=h*h*100*exp(-10*x);
     }
-    cout << "Calculation complete." << endl;
-    return 0;
+    //Special case diagonals
+    for(int i=1; i<=n; i++){
+        d_vec[i]=2;
+        a_vec[i]=-1;
+        c_vec[i]=-1;
+        //dt_vec[i]=0;
+        //ft_vec[i]=0;
+        //u_vec[i]=0;       //Initial values of u: u(0)=u(1)=0
+    }
+
+    dt_vec[1]=d_vec[1];
+    ft_vec[1]=f_vec[1];
+    u_vec[0]=0;
+    u_vec[n+1]=0;
+
+
+    start = clock();
+
+    //First for-loop to create b vector.
+    for(int i=2; i<=n; i++){
+        dt_vec[i]=d_vec[i] - c_vec[i-1]*a_vec[i-1]/dt_vec[i-1];
+        ft_vec[i]=f_vec[i] - ft_vec[i-1]*a_vec[i-1]/dt_vec[i-1];
+    }
+    u_vec[n]=ft_vec[n]/dt_vec[n];
+
+    //Compute u=f/d:j
+    for(int i=n; i>0; i--){
+        u_vec[i]=(ft_vec[i]-c_vec[i]*u_vec[i+1])/dt_vec[i];
+    }
+
+    finish = clock();
+    double clocks = (finish-start);
+    double timeElapsed = clocks/CLOCKS_PER_SEC;
+
+    cout << "N=10^" << i << ": " << timeElapsed << "s" << endl;
+
+    /*
+    //write file with current i and n:
+    filename.append(".txt");
+    ofile.open(filename);
+    //ofile << setiosflags(ios::showpoint | ios::uppercase);
+    for(double i=1; i<=n; i++){
+        double h;
+        h=1/(double(n)+1);
+        ofile << setprecision(10) << setw(20) << h*i;
+        ofile << setprecision(10) << setw(20) << 1-(1-exp(-10))*h*i - exp(-10*h*i);
+        ofile << setprecision(10) << setw(20) << u_vec[int(i)];
+        ofile << setprecision(10) << setw(20) << log10(fabs((- (1-(1-exp(-10))*h*i - exp(-10*h*i)) + u_vec[int(i)])/(1-(1-exp(-10))*h*i - exp(-10*h*i)))) << endl;
+    }
+    ofile.close();
+    */
+    delete [] d_vec; delete [] dt_vec; delete [] f_vec; delete [] ft_vec;
+    delete [] a_vec; delete [] c_vec; delete [] u_vec;
+  }
+  cout << "Calculation complete." << endl;
+}
+
+void LUDecompFunc(string fname, int maxexp){
+  clock_t start, finish;
+  string filename;
+  for (float i=0.5; i<=maxexp; i++){
+    filename = fname;
+    filename.append(to_string(i));
+    int n=pow(10.0,i);
+    double h;
+    h=1./(n+1);
+    mat A = zeros<mat>(n,n);
+
+    for(int i=0; i<n; i++){
+      A(i,i)=2;
+    }
+    for(int i=0; i<(n-1); i++){
+      A(i,i+1)=-1;
+      A(i+1,i)=-1;
+    }
+
+    vec b = zeros<vec>(n);
+    for(int i=0; i<n; i++){
+      b(i)=100*exp(-10*i*h)*h*h;
+    }
+
+    start = clock();
+
+    mat L, U;
+    vec x, z, u;
+    lu(L,U,A);
+
+    z = solve(L, b);
+    u = solve(U, z);
+
+
+    finish = clock();
+    double clocks = (finish-start);
+    double timeElapsed = clocks/CLOCKS_PER_SEC;
+
+    cout << "N=10^" << i << ": " << timeElapsed << "s" << endl;
+
+
+    /*
+    filename.append(".txt");
+    ofile.open(filename);
+    //ofile << setiosflags(ios::showpoint | ios::uppercase);
+    for(double i=1; i<n; i++){
+        double h;
+        h=1/(double(n)+1);
+        ofile << setprecision(10) << setw(20) << h*i;
+        ofile << setprecision(10) << setw(20) << 1-(1-exp(-10))*h*i - exp(-10*h*i);
+        ofile << setprecision(10) << setw(20) << u(int(i));
+        ofile << setprecision(10) << setw(20) << log10(fabs((- (1-(1-exp(-10))*h*i - exp(-10*h*i)) + u(int(i)))/(1-(1-exp(-10))*h*i - exp(-10*h*i)))) << endl;
+    }
+    ofile.close();
+    */
+  }
+
+
+  //AX=B
+  //X = solve(A,B)
 }
--- Project1/python_project1.py
+++ Project1/python_project1.py
@@ -10,8 +10,8 @@ h_list = []
 
 for i in range(1, exponent+1):
 	n = int(10**i)
-	h = 1./(n+1)
-	infile = open("results%i.txt" % n, "r")
+	h = -i
+	infile = open("results%i.txt" % i, "r")
 	steps = [0]
 	exact = [0]
 	comp  = [0]
@@ -28,96 +28,19 @@ for i in range(1, exponent+1):
 
 	maxerrors.append(max(err))
 	h_list.append(h)
-	'''
+
+	"""
 	plt.plot(steps, exact, "r.", label="Analytical solution")
 	plt.plot(steps, comp, "b.", label="Numerical solution")
 	plt.title("Analytical vs Numerical solution for $n=%i$" %n)
 	plt.legend(loc="best")
 	plt.show()
-	'''
+	"""
+
 
 plt.plot(h_list, maxerrors, "ro")
-plt.title("Max errors as a function of the step length.")
+plt.title("Logarithmic scale of max error as a function of the step length.")
+plt.xlabel("$log_{10}(h)$")
+plt.ylabel("$log_{10}(\\epsilon)$")
+plt.grid()
 plt.show()
-
-print(maxerrors)
-
-h_arr = np.asarray(h_list)
-me_arr = abs(np.asarray(maxerrors))
-
-print(h_arr, me_arr)
-
-lg_a = np.log10(h_arr)
-lg_m = np.log10(me_arr)
-
-plt.plot(lg_a, lg_m)
-plt.show()
-
-
-
-"""
-infile=open("results10.txt", "r")
-stepsize1=[]
-exactsol=[]
-compsol=[]
-err1=[]
-for line in infile:
-	a = line.split()
-	stepsize1.append(float(a[0]))
-	exactsol.append(float(a[1]))
-	compsol.append(float(a[2]))
-	err1.append(float(a[3]))
-infile.close()
-plt.plot(stepsize1, exactsol, "r.", label="Analytical solution")
-plt.plot(stepsize1, compsol, "b.", label="Numerical solution")
-plt.title("Analytical vs Numerical solution for $n=10$")
-plt.legend(loc="best")
-plt.show()
-
-
-infile=open("results1000.txt", "r")
-stepsize2=[]
-exactsol=[]
-compsol=[]
-err2=[]
-for line in infile:
-	a = line.split()
-	stepsize2.append(float(a[0]))
-	exactsol.append(float(a[1]))
-	compsol.append(float(a[2]))
-	err2.append(float(a[3]))
-infile.close()
-plt.plot(stepsize2, exactsol, "r.", label="Analytical solution")
-plt.plot(stepsize2, compsol, "b.", label="Numerical solution")
-plt.title("Analytic vs Numeric solution for $n=100$")
-plt.legend(loc="best")
-plt.show()
-
-
-
-infile=open("results1000.txt", "r")
-stepsize3=[]
-exactsol=[]
-compsol=[]
-err3=[]
-for line in infile:
-	a = line.split()
-	stepsize3.append(float(a[0]))
-	exactsol.append(float(a[1]))
-	compsol.append(float(a[2]))
-	err3.append(float(a[3]))
-infile.close()
-plt.plot(stepsize3, exactsol, "r.", label="Analitical solution")
-plt.plot(stepsize3, compsol, "b.", label="Numerical solution")
-plt.title("Analytic vs Numeric solution for $n=1000$")
-plt.legend(loc="best")
-plt.show()
-
-
-plt.plot(stepsize1, err1, label="Error for $n=10$")
-plt.plot(stepsize2, err2, label="Error for $n=100$")
-plt.plot(stepsize3, err3, label="Error for $n=1000$")
-plt.title("Absolute errors of the numerical calculations.")
-plt.legend(loc="best")
-plt.show()
-"""
