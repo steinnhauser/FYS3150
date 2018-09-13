c7aae85dbe6c37d3a8b630611ef64d082b8886b7
steinnhauser <steinnhauser@mac.com>
2018-09-13 13:43:50 +0200

    Started on a new function and added comments

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -14,15 +14,19 @@ const double zero = 10e-10;  //Constant which defines the zero point
 const double infi = 10e10;   //Same for infinity point
 const double pi = 3.141592;
 
-double analytic_eigenvalues(int N);
-int max_value_indexes(mat A, int N);
+double analytic_eigenvalues(int N); //Generates analytic eigenvalues using the definition from the exercise text
+int max_value_indexes(mat A, int N); //Returns indexes for max absolute value in matrix A, as (row, column)
+double generate_A_matrix(int N, vec a, vec d); //Generates the matrix A with diagonal vec d and upper and lower diagonals vec a.
 double t(double tau);
+
 
 int main(int argc, char* argv[])
 {
   int N=1000;
+  generate_A_matrix(int N, vec a, vec d);
   analytic_eigenvalues(N);
   double h=1/(N+1);
+
 
   return 0;
 }
@@ -39,9 +43,6 @@ double analytic_eigenvalues(int N){
   cout << lambda_analytic << endl;
 }
 
-
-}
-
 int max_value_indexes(mat A, int N){
   double maxval=0;
   int k, l;
@@ -54,6 +55,24 @@ int max_value_indexes(mat A, int N){
     }
   }
   return k, l
+}
+
+double generate_A_matrix(int N, vec a, vec d){
+  mat A = zeros<mat>((N-1),(N-1));
+  for (int i=0; i<(N-1); i++){ //This loop generates the diagonal
+    for (int j=0; j<(N-1); j++){
+      A(i, j) = d(i)
+    }
+  }
+
+  for (int i=0; i<(N-2); i++){ // This loop generates the upper and lower diagonals
+    for (int j=0; j<(N-2); j++){
+      A(i+1, j) = a(i)
+      A(i, j+1) = a(i)
+    }
+  }
+
+  return A;
 }
 
 double t(double tau) {
