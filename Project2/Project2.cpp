2d844ed12a988927f80d41bd2723648fd764875f
steinnhauser <steinnhauser@mac.com>
2018-09-13 13:17:39 +0200

    Added maxvalue index function

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -15,7 +15,7 @@ const double infi = 10e10;   //Same for infinity point
 const double pi = 3.141592;
 
 double analytic_eigenvalues(int N);
-double max_value_indexes(mat A, int N);
+int max_value_indexes(mat A, int N); //Returns indexes of max value in the form ([row], [column])
 
 int main(int argc, char* argv[])
 {
@@ -39,6 +39,17 @@ double analytic_eigenvalues(int N){
 
 
 }
-double max_value_indexes(mat A, int N){
+
+int max_value_indexes(mat A, int N){
+  double maxval=0;
+  int k, l;
+  for (int i=0; i<(N-1); i++){      //Loop over all rows of A.
+    for (int j=i+1; j<(N-1); i++){  //Loop from column i+1 to save time (symmetry)
+      val = A(i, j)
+      if (abs(val)>maxval){         //Check the absolute value, as it can have negative indexes
+        k=i; l=j;                   //Saving the indexes in values k and l
+      }
+    }
+  }
   return k, l
 }
