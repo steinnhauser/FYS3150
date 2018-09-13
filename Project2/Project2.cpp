eb187cc76982e2dc8a7deedb08cc6021e260fdfa
steinnhauser <steinnhauser@mac.com>
2018-09-13 12:52:58 +0200

    Nice skeleton code for Project2

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -14,23 +14,20 @@ const double zero = 10e-10;  //Constant which defines the zero point
 const double infi = 10e10;   //Same for infinity point
 const double pi = 3.141592;
 
-double Analytic_Eigenvalues(int N);
-double maxval();
+double analytic_eigenvalues(int N);
+double max_value_indexes(mat A, int N);
 
 int main(int argc, char* argv[])
 {
   cout << "Hello, World!" << endl;
   int N=1000;
-  //i am master
-  //I am Maxval branch
-  Analytic_Eigenvalues(N);
+  analytic_eigenvalues(N);
   double h=1/(N+1);
 
   return 0;
 }
 
-
-double Analytic_Eigenvalues(int N){
+double analytic_eigenvalues(int N){
   double h=1/(double(N)+1);
   double a = -1/(h*h);
   double d = 2/(h*h);
@@ -42,4 +39,7 @@ double Analytic_Eigenvalues(int N){
   cout << lambda_analytic << endl;
 
 
+}
+double max_value_indexes(mat A, int N){
+  return k, l
 }
