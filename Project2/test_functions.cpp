5b2e6a4634ee41d721eb8a1f3d8f24aeef7b3e6e
simehaa <simen.haapnes@gmail.com>
2018-09-21 13:37:40 +0200

    test of max value indices

--- Project2/test_functions.cpp
+++ Project2/test_functions.cpp
@@ -1,14 +1,22 @@
+#define CATCH_CONFIG_MAIN
 #include <armadillo>
+#include "catch.hpp"
 #include "test_functions.h"
 #include "functions.h"
 
 using namespace arma;
 
-
 void test_max_value_indexes() {
-  // generate some matrix 5x5 where indexes of max value is known
-
-
+  int N = 10;
+  vec a = ones<vec>(N-2) * -1.0; // Generating special case diagonal
+  vec d = ones<vec>(N-1) * 2.0;
+  mat A = generate_A_matrix(N, a, d);
+  A(2,4) = -5.0 // indices k = 2, l = 4
+  int k,l;
+  maxval = max_value_indexes(A,N,k,l);
+  REQUIRE( maxval == 5.0 );
+  REQUIRE( k == 2 );
+  REQUIRE( l == 4 );
 }
 
 void test_eigenvalues() {
