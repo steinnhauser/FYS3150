b14974af9b8ea590115a9c0eef0dde54246729ce
simehaa <simen.haapnes@gmail.com>
2018-10-31 14:45:16 +0100

    folder structure rough setup

---
+++ Project4/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  // command line inputs
+  // generate matrix
+
+  // temperature loop
+    // w
+    // metropolis call
+}
