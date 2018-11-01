f8361b46dcf4d4a98b4ca7f0c6826ef9dc30e0ce
steinnhauser <steinnhauser@mac.com>
2018-11-01 13:41:49 +0100

    Added a Boltzmann energy factor vector w to main

--- Project4/src/main.cpp
+++ Project4/src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "metropolis.h"
 #include "spin_initializer.h"
+#include <cmath>
 
 using namespace std;
 
@@ -14,19 +15,28 @@ int main(int argc, char* argv[]) {
   Initialize_spins(spin_matrix, L, false); // name, length and order: true=ordered, false=random
 
 
-  for (int i=0; i<L; i++)  {
-    for (int j=0; j<L; j++){
-      cout << spin_matrix[i][j] << " ";
-    }
-    cout << endl;
-  }
-
   double Tmin, Tmax;
   int MC_steps, T_step;
 
+  const double k = 1.381e-23;
+
   // body and soul of the program
   for (double temp=Tmin; temp<=Tmax; temp+=T_step) {
-    // w
+    double beta = 1./(k*temp);
+    double *w = new double [17]; // Boltzmann factors of energy levels -8J to 8J
+    for (int i=0; i<17; i++)
+    {
+      if (i%4==0)
+      {
+        w[i]=exp(-beta*(i-8));
+      } else w[i] = 0;
+    }
+
+    for (int i=0; i<17; i++)
+    {
+      std::cout << w[i] << std::endl;
+    }
+
     // metropolis call
   }
 
