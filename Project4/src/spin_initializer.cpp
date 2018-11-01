5267889668e55dec09ea5a94e4cc2195d89f359e
steinnhauser <steinnhauser@mac.com>
2018-11-01 12:53:31 +0100

    working on the initialize spin function

--- Project4/src/spin_initializer.cpp
+++ Project4/src/spin_initializer.cpp
@@ -1,4 +1,5 @@
 #include "../include/spin_initializer.h"
+#include <iostream>
 
 void Initialize_spins(int** matrixname, int L, bool order)
 {
@@ -12,18 +13,20 @@ void Initialize_spins(int** matrixname, int L, bool order)
     }
   } else { // initialize a random-spin matrix
     srand(time(NULL));
+    int r;
     for (int xs=0; xs<L; xs++)
     {
       for (int ys=0; ys<L; ys++)
       {
-        int r=rand() % 2; // either 0 or 1
-        if (r==0)
+        r=rand() % 2; // either 0 or 1
+        std::cout << r << std::endl;
+        if (r==1)
         {
-          matrixname[xs][ys]=-1;
+          matrixname[xs][ys]=1;
         }
         else
         {
-          matrixname[xs][ys]=1;
+          matrixname[xs][ys]=-1;
         }
       }
     }
