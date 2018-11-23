89741be5bfe1d11be0dc95a766819ff9c050b41c
simehaa <simen.haapnes@gmail.com>
2018-11-23 12:58:35 +0100

    include included

---
+++ Project5/include/functions.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <armadillo>
+
+using namespace arma;
+using namespace std;
+
+void explicitForwardEuler();
+void implicitBackwardEuler();
+void CrankNicolsonScheme();
+
+#endif // FUNCTIONS_H
