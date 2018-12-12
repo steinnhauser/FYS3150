8fc61fde2cc3820026d3404c0294ee3ce63d1611
simehaa <simen.haapnes@gmail.com>
2018-12-12 22:17:50 +0100

    lithosphere error plot

--- Project5/plotlithosphere.py
+++ Project5/plotlithosphere.py
@@ -11,19 +11,21 @@ def plot():
     files = [
         "data/lithosphere.bin",
         "data/lithosphere_Q.bin",
-        "data/lithosphere_QR1.bin"
+        "data/lithosphere_QR_equilfirst_noDecay.bin"
         ]
-    labels = ["Numerical: Q=0",
-        "Numerical: Q",
-        "Numerical: Q + subduction"
+    labels = ["Numerical: no heat",
+        "Numerical: natural heat",
+        "Numerical: subduction"
         ]
     colors = ['b-','y-','r-']
+    numerical = []
     nx = 126
     ny = 101
     nt = 100
     for file,label,c in zip(files,labels,colors):
         image = np.fromfile(file, dtype=float).reshape((ny,nx))
         temp = image[:,int(nx/2)]*1292 + 8
+        numerical.append(temp)
         depth = np.linspace(0,120,ny)
         plt.plot(depth,temp,c,label=label)
     plt.xlabel("Depth [km]")
@@ -31,37 +33,52 @@ def plot():
     plt.grid()
 
     # analytic: no heat production
-    x = np.linspace(0,120,121)
-    y = np.linspace(0,1,121)*1292 + 8
-    plt.plot(x,y,'b--',label="Analytic: Q=0")
+    analytic = []
+    x = np.linspace(0,120,101)
+    y = np.linspace(0,1,101)*1292 + 8
+    analytic.append(y)
+    plt.plot(x,y,'b--',label="Analytic: no heat")
 
     # analytic: natural heat production
-    xzones = [np.linspace(0,20,21),
-        np.linspace(20,40,21),
-        np.linspace(40,120,100)
-        ]
+    xzones = [x[:17],x[17:34],x[34:]]
     zones = [
         [-0.28,-23.66,8],
         [-0.07,-15.26,92],
         [-0.01,-10.46,188]
         ]
+    y2 = []
     for x,zone in zip(xzones,zones):
         y = np.polyval(zone,-x)
-        plt.plot(x,y,'y--',label="Analytic: Q")
+        y2.append(y)
+        plt.plot(x,y,'y--',label="Analytic: natural heat")
+    analytic.append(np.concatenate((y2[0],y2[1],y2[2])))
     # analytic: natural + subduction
-    xzones = [np.linspace(0,20,100),
-        np.linspace(20,40,100),
-        np.linspace(40,120,100)
-        ]
     zones = [
         [-0.28,-29,8],
         [-0.07,-20.6,92],
         [-0.11,-23.8,28]
         ]
+    y3 = []
     for x,zone in zip(xzones,zones):
         y = np.polyval(zone,-x)
-        plt.plot(x,y,'r--',label="Analytic: Q + subduction")
+        y3.append(y)
+        plt.plot(x,y,'r--',label="Analytic: subduction")
+    analytic.append(np.concatenate((y3[0],y3[1],y3[2])))
+    plt.legend()
+    plt.show()
 
+    # error plot
+    labels = ["Error: no heat",
+        "Error: natural heat",
+        "Error: subduction"
+        ]
+    x = np.linspace(0,120,101)
+    for a,n,lab in zip(analytic,numerical,labels):
+        plt.plot(x[1:-1],np.abs(a[1:-1]-n[1:-1])/a[1:-1],label=lab)
+    plt.yscale('log')
+    plt.xlabel('Depth [km]')
+    plt.ylabel('Relative error')
+    plt.grid()
     plt.legend()
     plt.show()
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FYS3150
-Computational Physics public repository.
+Computational Physics public repository. The projects are written as a collaboration of Steinn Hauser and Simen Haapnes. There are five projects:
+* Project 1: Poisson's equation in one dimension (Thomas algorithm and LU decomposition)
+* Project 2: Quantum mechanics - two electrons in a harmonic oscillator well (eigenvalue problems)
+* Project 3: Simulation of the solar system in 3D (Velocity Verlet method)
+* Project 4: Ising model in two dimensions (Metropolis algorithm - MCMC)
+* Project 5: Diffusion equation - heat diffusion in the lithosphere (Jacobi algorithm)
